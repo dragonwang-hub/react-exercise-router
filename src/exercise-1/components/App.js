e2ad2feaf827f8dbb11690e13104fa9da82e9c12
dragonwang-hub <915614879@qq.com>
2020-09-24 21:48:33 +0800

    [dragon]fix : refactor

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link, Switch, Redirect } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Home';
 import AboutUs from './AboutUs';
 import Profile from './Profile';
